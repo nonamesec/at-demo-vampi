1bf00baeb3dafc885a2b2be399ec2d8e338819e6
Zoe Meng <150256240+zmeng-teraform@users.noreply.github.com>
2025-10-19 20:51:35 +0800

    Disable vulnerable SQL query in get_user method
    Comment out SQL injection vulnerable code in get_user method.

--- models/user_model.py
+++ models/user_model.py
@@ -71,15 +71,15 @@ class User(db.Model):
         return done
 
     def get_user(username):
-        if vuln:  # SQLi Injection
-            user_query = f"SELECT * FROM users WHERE username = '{username}'"
-            query = db.session.execute(text(user_query))
-            ret = query.fetchone()
-            if ret:
-                fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
-            else:
-                fin_query = None
-        else:
+       # if vuln:  # SQLi Injection
+       #     user_query = f"SELECT * FROM users WHERE username = '{username}'"
+       #    query = db.session.execute(text(user_query))
+       #     ret = query.fetchone()
+       #     if ret:
+       #         fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
+       #     else:
+       #         fin_query = None
+       # else: 
             fin_query = User.query.filter_by(username=username).first()
         return fin_query
         
